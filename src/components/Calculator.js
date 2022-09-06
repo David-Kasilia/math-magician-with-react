acb4f25265029f4d9b140353f8421af1bd008410
David Kasilia <83514256+David-Kasilia@users.noreply.github.com>
2022-09-06 19:56:20 +0300

    Complete Calculator ui

---
+++ src/components/Calculator.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import '../App.css';
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="calculator-grid">
+        <div className="output">
+          <div className="previous-operand" />
+          <div className="current-operand">
+            0
+          </div>
+        </div>
+
+        <button type="button" className="btn">AC</button>
+        <button type="button" className="btn">+/-</button>
+        <button type="button" className="btn">%</button>
+        <button type="button" className="btn1">÷</button>
+        <button type="button" className="btn">7</button>
+        <button type="button" className="btn">8</button>
+        <button type="button" className="btn">9</button>
+        <button type="button" className="btn1">×</button>
+        <button type="button" className="btn">4</button>
+        <button type="button" className="btn">5</button>
+        <button type="button" className="btn">6</button>
+        <button type="button" className="btn1">-</button>
+        <button type="button" className="btn">1</button>
+        <button type="button" className="btn">2</button>
+        <button type="button" className="btn">3</button>
+        <button type="button" className="btn1">+</button>
+        <button type="button" className="span-two">0</button>
+        <button type="button" className="btn">.</button>
+        <button type="button" className="btn1">=</button>
+
+      </div>
+    );
+  }
+}
+
+export default Calculator;
